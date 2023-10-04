642d72a2f0acd33318e3b4cbf643ecc3d3415aec
ivan <ivan@DESKTOP-NELLSAH>
2023-10-04 22:11:26 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const num =
+        "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(
+            " "
+        );
+    const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(
+        " "
+    );
+    let digit = number % 10;
+
+    if (number < 20) {
+        return num[number];
+    } else if (number < 100) {
+        return tens[~~(number / 10) - 2] + (digit ? " " + num[digit] : "");
+    } else if (number < 1000) {
+        return (
+            num[~~(number / 100)] +
+            " hundred" +
+            (number % 100 == 0 ? "" : " " + toReadable(number % 100))
+        );
+    } else {
+        return (
+            toReadable(~~(number / 1000)) +
+            " thousand" +
+            (number % 1000 != 0 ? " " + toReadable(number % 1000) : "")
+        );
+    }
+};
